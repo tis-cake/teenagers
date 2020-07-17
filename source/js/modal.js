56a2769bb4265a92ccc02fdadb4a426d69fd3808
tis <tisbootis@gmail.com>
2020-07-17 22:42:26 +0300

    add fix for styles and js in rewiews block; add basic css-fix

--- source/js/modal.js
+++ source/js/modal.js
@@ -1,18 +1,18 @@
-$('body').click(function(e){
-    if($(e.target).is($('.modal'))){
-        $('.modal').removeClass('show');
-    }
+$('body').click(function(e) {
+  if ($(e.target).is($('.modal'))) {
+    $('.modal').removeClass('show');
+  }
 });
 
 
 // data-toggle="modal" data-modal-id="submit_your_application"
-$('*[data-toggle="modal"]').on('click', function () {
+$('*[data-toggle="modal"]').on('click', function() {
 
-    let _this_modal = $(this).data('modal-id');
-    $('.modal').removeClass('show');
-    $('#' + _this_modal).addClass('show');
+  let _this_modal = $(this).data('modal-id');
+  $('.modal').removeClass('show');
+  $('#' + _this_modal).addClass('show');
 
 });
-$('.close-modal').on('click', function () {
-    $(this).parents('.modal').removeClass('show');
+$('.close-modal').on('click', function() {
+  $(this).parents('.modal').removeClass('show');
 });
